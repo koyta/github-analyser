bc212b136345766244ddb1188e341d0548b34a85
Andrey Khlivnyuk <koytaboyta@gmail.com>
2018-03-03 22:29:37 +0300

    замена shallowobject на shallowmap

--- src/store.js
+++ src/store.js
@@ -1,10 +1,4 @@
-import {
-  action,
-  computed,
-  extendShallowObservable,
-  observable,
-  useStrict,
-} from 'mobx'
+import { action, computed, observable, useStrict, extendShallowObservable } from 'mobx'
 import { apiCall } from './utils/apiCall'
 
 useStrict(true)
@@ -15,14 +9,14 @@ class GithubStore {
 
   @observable _repos = []
   @observable _repoLanguages = []
-  // @observable _overallLanguages = observable.shallowObject({})
-  @observable _overallLanguages = observable.shallowMap({})
-
 
   @computed
   get repoLanguages () {
     return this._repoLanguages
   }
+
+  // @observable _overallLanguages = observable.shallowObject({})
+  @observable _overallLanguages = observable.shallowMap({})
 
   @computed
   get overallLanguages () {
@@ -31,13 +25,19 @@ class GithubStore {
 
   @computed
   get languagesSummary () {
-    let temp = []
-    for (let lang in this._overallLanguages) {
-      if (this._overallLanguages.hasOwnProperty(lang)) { //if (this._overallLanguages have "JavaScript" key)
+    let temp = new Map()
+    this._overallLanguages.forEach((lang, i, map) => {
+      if (map.has(lang)) {
         console.log(lang)
-        temp.push(this._overallLanguages[lang])
+        temp.set(lang, map.get(lang))
       }
-    }
+    })
+    // for (let lang in this._overallLanguages) {
+    //   if (this._overallLanguages.hasOwnProperty(lang)) { //if (this._overallLanguages have "JavaScript" key)
+    //     console.log(lang)
+    //     temp.push(this._overallLanguages[lang])
+    //   }
+    // }
     let sum = 0
     temp.forEach((item) => {
       sum += item
@@ -60,16 +60,26 @@ class GithubStore {
       })
       this._repoLanguages = await Promise.all(langs)
       this._repoLanguages.forEach(item => {
-        let langs = item.languages
-        Object.keys(langs).forEach((lang) => {
-          if (lang in this._overallLanguages) {
-            this._overallLanguages[lang] += langs[lang]
+        let langs = item.language
+        Object.keys(langs).forEach(lang => {
+          if (this._overallLanguages.has(lang)) {
+            this._overallLanguages.set(lang, langs[lang])
           } else {
             extendShallowObservable(this._overallLanguages, {
-              [lang]: langs[lang],
+              [lang]: langs[lang]
             })
           }
         })
+        // let langs = item.languages
+        // Object.keys(langs).forEach((lang) => {
+        //   if (lang in this._overallLanguages) {
+        //     this._overallLanguages[lang] += langs[lang]
+        //   } else {
+        //     extendShallowObservable(this._overallLanguages, {
+        //       [lang]: langs[lang],
+        //     })
+        //   }
+        // })
       })
       this.state = 'done'
     } catch (error) {

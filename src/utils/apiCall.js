057b3f732a03e03af1cccfe6df4fc7fa80ce3be3
Andrey Khlivnyuk <koytaboyta@gmail.com>
2018-03-05 19:23:46 +0300

    remove apiCall params

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -2,7 +2,4 @@ import axios from 'axios';
 
 export const apiCall = axios.create({
   baseURL: 'https://api.github.com',
-  params: {
-    access_token: 'e30c08eec08b5701b50c75b8bb8031d9cdaf7cee',
-  },
 });
